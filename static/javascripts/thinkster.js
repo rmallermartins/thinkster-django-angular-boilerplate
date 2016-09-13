5fe34f9636e4a3f03e738adfa3880a09e9ab5f4f
Rodrigo Maller Martins <rmallermartins@gmail.com>
2016-09-13 15:54:16 -0300

    First Commit, up to Registration

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,23 @@
-angular
-  .module('thinkster', []);
+(function functionName() {
+  'use strict';
+
+  angular.module('thinkster', [
+    'thinkster.routes',
+    'thinkster.config',
+    'thinkster.authentication'
+  ]);
+
+  angular.module('thinkster.routes', ['ngRoute']);
+
+  angular.module('thinkster.config', []);
+
+  // CSRF Protection
+  angular.module('thinkster').run(run);
+
+  run.$inject = ['$http'];
+
+  function run($http) {
+    $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $http.defaults.xsrfCookieName = 'csrftoken';
+  }
+})();
--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,18 @@
-from django.conf.urls import patterns, url
+from rest_framework import routers
+
+from django.conf.urls import patterns, url, include
+
+from authentication.views import AccountViewSet
 
 from thinkster_django_angular_boilerplate.views import IndexView
 
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
 urlpatterns = patterns(
     '',
+    url(r'^api/v1/', include(router.urls)),
 
     url('^.*$', IndexView.as_view(), name='index'),
 )
